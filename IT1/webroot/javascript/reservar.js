3dc47356cf0f722dc002ec4e3ad13f25b687c861
beatrizcn7 <97543271+beatrizcn7@users.noreply.github.com>
2022-05-09 17:42:00 +0100

    a

--- IT1/webroot/javascript/reservar.js
+++ IT1/webroot/javascript/reservar.js
@@ -1,69 +1,63 @@
 function place(zona){
   var checkBox = document.getElementById(zona);
-  lugarA = document.getElementById("lugarA");
-  lugarB = document.getElementById("lugarB");
-  lugarC = document.getElementById("lugarC");
-  lugarD = document.getElementById("lugarD");
-  lugarE = document.getElementById("lugarD");
-  lugarF = document.getElementById("lugarD");
     if (checkBox.checked === true){
         switch(zona){
             case 'za':
-                lugarA.style.display = "block";
-                lugarB.style.display = "none";
-                lugarC.style.display = "none";
-                lugarD.style.display = "none";
-                lugarE.style.display = "none";
-                lugarF.style.display = "none";
+                document.getElementById("lugarA").style.display = "block";
+                document.getElementById("lugarB").style.display = "none";
+                document.getElementById("lugarC").style.display = "none";
+                document.getElementById("lugarD").style.display = "none";
+                document.getElementById("lugarE").style.display = "none";
+                document.getElementById("lugarF").style.display = "none";
             case 'zb':
-                lugarA.style.display = "none";
-                lugarB.style.display = "block";
-                lugarC.style.display = "none";
-                lugarD.style.display = "none";
-                lugarE.style.display = "none";
-                lugarF.style.display = "none";
+                document.getElementById("lugarA").style.display = "none";
+                document.getElementById("lugarB").style.display = "block";
+                document.getElementById("lugarC").style.display = "none";
+                document.getElementById("lugarD").style.display = "none";
+                document.getElementById("lugarE").style.display = "none";
+                document.getElementById("lugarF").style.display = "none";
             case 'zc':
-                lugarA.style.display = "none";
-                lugarB.style.display = "none";
-                lugarC.style.display = "block";
-                lugarD.style.display = "none";
-                lugarE.style.display = "none";
-                lugarF.style.display = "none";
-            case 'zd':
-                lugarA.style.display = "none";
-                lugarB.style.display = "none";
-                lugarC.style.display = "none";
-                lugarD.style.display = "block";
-                lugarE.style.display = "none";
-                lugarF.style.display = "none";
+                document.getElementById("lugarA").style.display = "none";
+                document.getElementById("lugarB").style.display = "none";
+                document.getElementById("lugarC").style.display = "block";
+                document.getElementById("lugarD").style.display = "none";
+                document.getElementById("lugarE").style.display = "none";
+                document.getElementById("lugarF").style.display = "none";
+            case'zd':
+                document.getElementById("lugarA").style.display = "none";
+                document.getElementById("lugarB").style.display = "none";
+                document.getElementById("lugarC").style.display = "none";
+                document.getElementById("lugarD").style.display = "block";
+                document.getElementById("lugarE").style.display = "none";
+                document.getElementById("lugarF").style.display = "none";
             case 'ze':
-                lugarA.style.display = "none";
-                lugarB.style.display = "none";
-                lugarC.style.display = "none";
-                lugarD.style.display = "none";
-                lugarE.style.display = "block";
-                lugarF.style.display = "none";
+                document.getElementById("lugarA").style.display = "none";
+                document.getElementById("lugarB").style.display = "none";
+                document.getElementById("lugarC").style.display = "none";
+                document.getElementById("lugarD").style.display = "none";
+                document.getElementById("lugarE").style.display = "block";
+                document.getElementById("lugarF").style.display = "none";
             case 'zf':
-                lugarA.style.display = "none";
-                lugarB.style.display = "none";
-                lugarC.style.display = "none";
-                lugarD.style.display = "none";
-                lugarE.style.display = "none";
-                lugarF.style.display = "block";
+                document.getElementById("lugarA").style.display = "none";
+                document.getElementById("lugarB").style.display = "none";
+                document.getElementById("lugarC").style.display = "none";
+                document.getElementById("lugarD").style.display = "none";
+                document.getElementById("lugarE").style.display = "none";
+                document.getElementById("lugarF").style.display = "block";
             default:
-                lugarA.style.display = "none";
-                lugarB.style.display = "none";
-                lugarC.style.display = "none";
-                lugarD.style.display = "none";
-                lugarE.style.display = "none";
-                lugarF.style.display = "none";
+                document.getElementById("lugarA").style.display = "none";
+                document.getElementById("lugarB").style.display = "none";
+                document.getElementById("lugarC").style.display = "none";
+                document.getElementById("lugarD").style.display = "none";
+                document.getElementById("lugarE").style.display = "none";
+                document.getElementById("lugarF").style.display = "none";
         }
     }else {
-        lugarA.style.display = "none";
-        lugarB.style.display = "none";
-        lugarC.style.display = "none";
-        lugarD.style.display = "none";
-        lugarE.style.display = "none";
-        lugarF.style.display = "none";
+        document.getElementById("lugarA").style.display = "none";
+        document.getElementById("lugarB").style.display = "none";
+        document.getElementById("lugarC").style.display = "none";
+        document.getElementById("lugarD").style.display = "none";
+        document.getElementById("lugarE").style.display = "none";
+        document.getElementById("lugarF").style.display = "none";
     }
 }
